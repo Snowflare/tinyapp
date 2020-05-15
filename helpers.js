a20bf318bf10fea81739b02daddc71efcc7580c1
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-15 03:38:35 +0000

    helpers.js linted

--- helpers.js
+++ helpers.js
@@ -2,60 +2,60 @@ const bcrypt = require('bcrypt');
 // Helper functions
 //------------------------------------------------/
 // Random string generator
-function generateRandomString() {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for ( var i = 0; i < 6; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+const generateRandomString = function() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
-}
+};
 // Return user object by email
 const getUserByEmail = function(email, database) {
-  for (let user in database){
-    if (database[user].email === email){
+  for (let user in database) {
+    if (database[user].email === email) {
       return database[user];
     }
   }
   return undefined;
 };
 // Check if the email and password match
-function isPassword(email, password, users){
-  for (let user in users){
-    if (users[user].email === email && bcrypt.compareSync(password, users[user].password)){
+const isPassword = function(email, password, users) {
+  for (let user in users) {
+    if (users[user].email === email && bcrypt.compareSync(password, users[user].password)) {
       return true;
     }
   }
   return false;
-}
+};
 // Return the urls belongs to given id
-function filter( dataBase, id) {
+const filter = function(dataBase, id) {
   let result = {};
 
-  for (key in dataBase) {
-      if (dataBase.hasOwnProperty(key) && dataBase[key].user_id === id) {
-          result[key] = dataBase[key];
-      }
+  for (let key in dataBase) {
+    if (dataBase[key] && dataBase[key].user_id === id) {
+      result[key] = dataBase[key];
+    }
   }
 
   return result;
 };
 // Check if user exists in the database
-function isUser(id, users){
-  if(users[id]){
+const isUser = function(id, users) {
+  if (users[id]) {
     return true;
   }
   return false;
-}
+};
 
 // Check if email exists in the database
-function isEmail(email, users){
-  for (let user in users){
-    if (users[user].email === email){
+const isEmail = function(email, users) {
+  for (let user in users) {
+    if (users[user].email === email) {
       return true;
     }
   }
   return false;
-}
+};
 
 module.exports = {getUserByEmail, isEmail, isUser, isPassword, generateRandomString, filter};
